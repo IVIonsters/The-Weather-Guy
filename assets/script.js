95ffd8a003643ebc226f4a9507b271c70f983ca8
Zachary Polof <ZacharyPolof@gmail.com>
2024-05-04 15:29:18 -0500

    .html .css .js changes
    removed column-sm from container , added calss to search button
    
    changed main html horizontal line styling
    
    defined variables for JS and added API
    
    added call for render page

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,14 @@
+// API KEY: for weather access:
 const APIKey = "78506e3c00a53204a2691e069931f836";
+
+//Define variables for use in functions.
+let userCity = document.querySelector("#searchCity");
+let searchButton = document.querySelector(".searchButton");
+let weatherForecast = document.querySelector(".forecastToday");
+let cityHistory = document.querySelector(".searchHistory");
+let cardRender = document.querySelector(".renderCards");
+
+// call to retrieve local storage and render cards
+renderCityHistory();
+
+// Fetch request to OpenWeather API
